a402924acad166f4bfbb8f0cd17d872a77c2e7d2
camobrien343 <camobrien343@gmail.com>
2018-06-05 13:35:13 -0500

    removed alphabetize

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     plugins: [
         require('autoprefixer'),
-        require('postcss-sort-alphabetically'),
         require('cssnano')({
             preset: 'default',
             reduceIdents: false
